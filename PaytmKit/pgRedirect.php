d33abf846b89f9e40a3d79996adabca9b8349c0e
Manish <manish.sah.cs19@ggits.net>
2023-07-06 21:50:35 +0530

    bug fixed

--- PaytmKit/pgRedirect.php
+++ PaytmKit/pgRedirect.php
@@ -17,8 +17,8 @@ $CUST_ID = $_POST["CUST_ID"];
 $INDUSTRY_TYPE_ID = $_POST["INDUSTRY_TYPE_ID"];
 $CHANNEL_ID = $_POST["CHANNEL_ID"];
 $TXN_AMOUNT = $_POST["TXN_AMOUNT"];
-$MSISDN = $_POST["MSISDN"]; 
-$EMAIL = $_POST["EMAIL"]; 
+// $MSISDN = $_POST["MSISDN"]; 
+// $EMAIL = $_POST["EMAIL"]; 
 // Create an array having all required parameters for creating checksum.
 $paramList["MID"] = PAYTM_MERCHANT_MID;
 $paramList["ORDER_ID"] = $ORDER_ID;
@@ -28,8 +28,8 @@ $paramList["CHANNEL_ID"] = $CHANNEL_ID;
 $paramList["TXN_AMOUNT"] = $TXN_AMOUNT;
 $paramList["WEBSITE"] = PAYTM_MERCHANT_WEBSITE;
 $paramList["CALLBACK_URL"] = "http://localhost/paytm/PaytmKit/pgResponse.php";
-$paramList["MSISDN"] = $MSISDN; //Mobile number of customer
-$paramList["EMAIL"] = $EMAIL; //Email ID of customer
+// $paramList["MSISDN"] = $MSISDN; //Mobile number of customer
+// $paramList["EMAIL"] = $EMAIL; //Email ID of customer
 /*
 $paramList["VERIFIED_BY"] = "EMAIL"; //
 $paramList["IS_USER_VERIFIED"] = "YES"; //
